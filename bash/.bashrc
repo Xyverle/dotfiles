86a984463ea79adaba3592e982b3b45cbb888d36
Xyverle <xyverle.public@gmail.com>
2025-06-29 22:59:33 -0400

    Init

---
+++ bash/.bashrc
@@ -0,0 +1,54 @@
+# Add ~/.cargo/bin/ and ~/.local/bin to PATH
+export PATH="~/.local/bin:$PATH"
+export PATH="~/.cargo/bin:$PATH"
+
+[[ $- != *i* ]] && return # Don't continue if not interactive
+
+export HISTCONTROL=ignorespace
+export BROWSER=librewolf
+# pfetch settings
+# note: here I am actually using pfetch-rs and not pfetch for a faster runtime https://github.com/Gobidev/pfetch-rs
+# if you want to use something other than pfetch remove first 2 lines and replace FETCH with your preferred fetch
+export PF_INFO="title ascii os host kernel uptime pkgs memory"
+export PF_COL3=1
+export FETCH='pfetch'
+alias clear="clear; $FETCH"
+$FETCH 
+
+# eza is a replacement for ls that gives it color, icons, etc... https://github.com/eza-community/eza
+alias l="eza -a -l -s type --icons --no-permissions"
+# hyperfine is a replacement for time that runs multiple tests and gives a summary https://github.com/sharkdp/hyperfine
+alias time="hyperfine"
+# zoxide is a replacement for cd that remembers path you've visited and lets you use shortcuts https://github.com/ajeetdsouza/zoxide
+# optionally use can install fzf for fuzzy finding when using interactive mode https://github.com/junegunn/fzf
+eval "$(zoxide init bash --cmd j)"
+#                              ^ I have this bound to j change this to cd if you prefer that
+
+# zellij is a rust replacement for tmux
+#eval "$(zellij setup --generate-auto-start bash)"
+
+# jump and list
+jl(){
+  j $1; l
+}
+
+# this colors the dollar sign green on command success/ctrl+c termination and red on failure(it also gives the exit code on failure)
+colorPrompt(){
+  cmdstatus="$?"
+  if [ "$cmdstatus" -eq 0 ] || [ "$cmdstatus" -eq 130 ]; then
+      printf "\033[32m$PROMPT_END"
+    else
+      printf "\033[31m$cmdstatus$PROMPT_END"
+  fi
+}
+
+export PROMPT_END="$"
+export PROMPT_DIR_COLOR="\033[34m"
+
+# configures the prompt
+# if you want to configure this simply put \[$(colorPrompt)\] before your dollar sign, arrow, etc...
+export PS1='\033[34m\w \[$(colorPrompt)\]\033[0m '
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
---
+++ nvim/init.lua
@@ -0,0 +1,114 @@
+-- Install plugins
+-- Using paq
+-- See https://github.com/savq/paq-nvim
+require "paq" {
+    "savq/paq-nvim",
+    'kyazdani42/nvim-web-devicons',
+    "windwp/nvim-autopairs",
+    "numToStr/Comment.nvim",
+    "alvarosevilla95/luatab.nvim",
+    "nvim-lualine/lualine.nvim",
+    { "catppuccin/nvim", as = "catppuccin" },
+}
+
+-- Configure plugins
+require("lualine").setup()
+require("Comment").setup()
+require("luatab").setup()
+require("nvim-autopairs").setup()
+
+-- Keymaps
+local opts = { noremap = true, silent = true}
+local keymap = vim.api.nvim_set_keymap
+vim.g.mapleader = ","
+vim.g.maplocalleader = ","
+keymap("n", "<Leader>f", ":Ex<CR>", opts)
+keymap("n", "<Leader>t", ":tabnew<CR>:Ex<CR>", opts)
+keymap("n", "<Leader>v", ":vnew<CR>:Ex<CR>", opts)
+keymap("n", "<Leader>h", ":new<CR>:Ex<CR>", opts)
+keymap("n", "<Tab>", ":tabn<CR>", opts)
+keymap("n", "<S-Tab>", ":tabp<CR>", opts)
+
+-- comment for default bindings
+keymap("", "j", "<Left>", opts)
+keymap("", "k", "<Down>", opts)
+keymap("", "l", "<Up>", opts)
+keymap("", ";", "<Right>", opts)
+keymap("n", "<C-j>", "<C-W>h", opts)
+keymap("n", "<C-k>", "<C-W>j", opts)
+keymap("n", "<C-l>", "<C-W>k", opts)
+keymap("n", "<C-;>", "<C-W>l", opts)
+keymap("n", "H", ":%s//<Left>", opts)
+keymap("n", "h", ":%s//g<Left><Left>", opts)
+
+-- uncomment for default bindings
+-- keymap("n", "<C-h>", "<C-W>h", opts)
+-- keymap("n", "<C-k>", "<C-W>k", opts)
+-- keymap("n", "<C-j>", "<C-W>j", opts)
+-- keymap("n", "<C-l>", "<C-W>l", opts)
+-- keymap("n", "G", ":%s//<Left>", opts)
+-- keymap("n", "g", ":%s//g<Left><Left>", opts)
+
+-- Configure nvim
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.autoindent = true
+
+vim.opt.hlsearch = false
+vim.opt.wrap = false
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+
+vim.opt.backspace = "indent,eol,start"
+vim.opt.clipboard:append("unnamedplus")
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+vim.cmd("colorscheme catppuccin")
+vim.opt.laststatus = 3
+vim.opt.relativenumber = true
+vim.opt.number = true
+
+vim.diagnostic.config({ virtual_text = true })
+vim.o.completeopt = "menu,menuone,noselect"
+vim.o.pumheight = 10
+
+-- LSPs
+
+-- Rust
+
+vim.lsp.config('rust-analyzer', {
+    cmd = {'rust-analyzer'},
+    filetypes = {'rust'},
+    root_markers = {'Cargo.toml', '.git'},
+})
+vim.lsp.enable("rust-analyzer")
+
+-- General
+
+function autoformat(client, args)
+    if client:supports_method('textdocument/formatting') then
+        vim.api.nvim_create_autocmd('BufWritePre', {
+            buffer = args.buf,
+            callback = function()
+            vim.lsp.buf.format({ bufnr = args.buf, id = client.id, timeout_ms = 1000 })
+        end,
+    })
+    end
+end
+
+function autocomplete(client, args)
+    if client:supports_method('textDocument/completion') then
+        vim.lsp.completion.enable(true, client.id, args.buf, { autotrigger = true })
+    end
+end
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(args)
+        local client = assert(vim.lsp.get_client_by_id(args.data.client_id))
+        autoformat(client, args)
+        autocomplete(client, args)
+    end,
+})
